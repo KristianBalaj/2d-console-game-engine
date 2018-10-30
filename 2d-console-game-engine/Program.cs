9ce15be865bc87001d04f7dd1a606619cbe21a1b
Kristian Balaj <kiko.balaj@gmail.com>
2018-10-30 13:15:06 +0100

    Empty VS project created

---
+++ 2d-console-game-engine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2d_console_game_engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
